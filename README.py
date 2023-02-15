168fb9a350e52b56c6b471729b0aeb58ef9f9259
Miguel Campo <125380860+McMac31@users.noreply.github.com>
2023-02-15 17:54:04 +0100

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,39 @@
+# ActividadAvanzada2
+#Lista
+
+Compras = ["Leche", "Arroz", "Aceite", "Lentejas"]
+#Paso 1
+Compras.append("Zumo")
+Compras.append("Galletas")
+#Paso 2
+print(Compras)
+#Paso 3
+print(Compras[1:3])
+#Paso 4
+print(Compras[4:6])
+#Paso 5
+Compras[4]= "ZUMO DE NARANJA"
+#Paso 6
+Compras.pop()
+# #Paso 7
+Compras.append("Limones")
+Compras.append("Naranjas")
+Compras.append("Macarrones")
+Compras.append("Café")
+Compras.append("Pipas")
+Compras.append("Colacao")
+#Paso 8
+Compras.sort() 
+
+#Tuple
+
+ #Paso 1
+Estaciones=("Primavera","Verano","Otoño","Invierno")
+print(Estaciones[1:2])
+#Paso 2
+print(Estaciones)
+
+#Actividad avanzada
+nombres = ["Arturo","Julio","Dani"]
+for i in nombres:
+    Estudiantes= input(f"Ha venido {nombres} a clase? y/n")
